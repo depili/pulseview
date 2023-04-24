depili/pulseview

commit 292b6ebc37281a28982e5d9e43471d9b3eab1d8f

StoreSession: Use Glib::DateTime

pv/storesession.cpp
@@ -185,9 +185,7 @@ bool StoreSession::start()
 				any_segment->samplerate())}});
 		output_->receive(meta);
 
-		Glib::TimeVal start_time;
-		start_time.tv_sec = 0;  // TODO Assumes saved data begins at t=0
-		start_time.tv_usec = 0;
+		Glib::DateTime start_time;
 		auto header = context->create_header_packet(start_time);
 		output_->receive(header);
 	} catch (Error& error) {

commit 4bee23ee5a76b9ecb068e39a6ba82ccb86e430c2

Fix regression compiling with glibmm-2.66
Commit 292b6ebc37281a28982e5d9e43471d9b3eab1d8f introduced a regression
for compiling pulseview with older glibmm versions while fixing the build
on glibmm-2.76, which has dropped Glib:TimeVal.

This just puts the previous code inside a glibmm version check, some
better work around might be available for the previous glibmm versions,
as they have DateTime, but fail on compile due to conversion errors:

pulseview/pv/storesession.cpp:191:61: error: cannot convert ‘Glib::TimeVal’ to'
‘Glib::DateTime’"

pv/storesession.cpp
@@ -185,7 +185,17 @@ bool StoreSession::start()
 				any_segment->samplerate())}});
 		output_->receive(meta);
 
+#if GLIB_CHECK_VERSION(2, 76, 0)
+		// glibmm-2.76.0 doesn't have TimeVal anymore
 		Glib::DateTime start_time;
+
+#else
+		// Glibmm-2.66 doesn't compile with DateTime
+     	Glib::TimeVal start_time;
+       	start_time.tv_sec = 0;  // TODO Assumes saved data begins at t=0
+		start_time.tv_usec = 0;		auto header = context->create_header_packet(start_time);
+#endif
+
 		auto header = context->create_header_packet(start_time);
 		output_->receive(header);
 	} catch (Error& error) {